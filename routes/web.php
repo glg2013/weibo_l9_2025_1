5ed7711188d785124c8b9f3699d99c71ee6ec0c5
vagrant <vagrant@homestead>
2025-01-05 15:55:44 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StaticPagesController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,5 @@ Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
+
+Route::get('signup', [UsersController::class, 'create'])->name('signup');
