16adfdd4bfb1264f114e83ddc1e06c91cc18e314
vagrant <vagrant@homestead>
2025-02-17 00:21:40 +0000

    粉丝数据表

---
+++ database/migrations/2025_02_16_202732_create_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->index();         // 被关注的用户
+            $table->bigInteger('follower_id')->index();     // 粉丝用户
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+};
